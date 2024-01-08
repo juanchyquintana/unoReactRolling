46980065bd319d237bf57130187214b60becf856
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-01-08 14:25:24 -0300

    se creó el componente SaludoPrincipal del ejercicio numero 1

---
+++ src/components/SaludoPrincipal.jsx
@@ -0,0 +1,11 @@
+
+
+const SaludoPrincipal = () => {
+  return (
+    <>
+        <h1 className="text-center display-1 fw-bold text-uppercase ">Hello World!</h1>
+    </>
+  )
+}
+
+export default SaludoPrincipal
